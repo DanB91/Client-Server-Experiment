b9d3505b494bfbd0af120d3fca6ad4a9cdf9b80d
Daniel Bokser <dan.bokser@gmail.com>
2022-10-03 22:43:43 -0400

    added html and go funcs

--- go/main.go
+++ go/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+)
 
+func main() {
+	println("Hello!!!")
+}
+
+func println(f string, args ...any) {
+	fmt.Printf(f+"\n", args...)
+}
+
+func fatal(f string, args ...any) {
+	println(f, args...)
+	os.Exit(1)
 }
